8133eee4bd4a8795cdd4871311238a686e93384b
leave3310 <sampisam3@gmail.com>
2022-03-23 18:48:14 +0800

    add waterfall animation

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-const API_URL = 'https://hex-escape-room.herokuapp.com'
+$(document).ready(function () {
+    const bricklayer = new Bricklayer(document.querySelector('.bricklayer'));
 
-const showData = (data) => {
+    const imgSrc = [
+        './images/1 - ArsdCGv.jpg', './images/2 - LNUXzZF.jpg',
+        './images/3 - kgTEVHZ.jpg', './images/4 - vZoqs0P.jpg',
+        './images/5 - SEu9pHo.jpg', './images/6 - OkKwWM9.jpg',
+        './images/7 - eUppgFB.jpg', './images/8 - WAIXTXs.jpg',
+        './images/9 - vb0uajo.jpg', './images/10 - s6w2RpD.jpg'
+    ]
 
-    data.forEach(item => {
-        item.publishedAt = moment.tz(item.publishedAt, item.iana).format("YYYY-MM-DD HH:mm:ss")
-    })
-    axios.post(`${API_URL}/api/news/v1/data`, {
-        data: data
-    }).then(res => console.log(res.data))
-}
+    const renderData = (src) => {
+        const node = document.createElement('li')
+        node.className = "mb-5 grid-item bounce-in"
+        node.innerHTML =
+            `<img src="${src}" alt="">`
+        return node
+    }
+    
+    const render = () => {
+        imgSrc.forEach((item, index) => {
+            setTimeout(() => {
+                bricklayer.append(renderData(item))
+            }, index * 1000)
+        })
+    }
 
-axios.get(`${API_URL}/api/news/v1/data`)
-    .then(res => {
-        showData(res.data.articles)
-    })
-    .catch(err => err)
+    render()
+
+})
