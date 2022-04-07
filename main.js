e6eb0af74d6b13c9cab8620206bc37b85e65cf95
leave3310 <sampisam3@gmail.com>
2022-04-07 10:01:58 +0800

    Add tepojson

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const { HmacSHA256 } = CryptoJS;
-const { Base64 } = CryptoJS.enc;
+const URL = 'https://hexschool.github.io/tw_revenue/tw_revenue.json'
+const geoJson = './COUNTY_MOI_1090820.json'
+let taiwanTopo = []
 
-const URL = "https://hex-escape-room.herokuapp.com/api/linePay/v3/payments/request"
+const drawMap = (mapData) => {
+    //
+}
 
-const secret = 'OPbD1Pr6ClcPXvd9BHHso1EvhARV0iEz';
-const channelId = '1234567890';
-const uri = '/v3/payments/request';
-const nonce = Math.floor(Date.now() / 1000);
-const body = {
-    "amount": 100,
-    "currency": "TWD",
-    "orderId": "xgmks1639467061",
-};
+const fetchTopo = () => {
+    fetch(geoJson)
+        .then(res => res.json())
+        .then(res => {
+            taiwanTopo = res
+        })
+        .then(mapData => {
+            // 定義經緯度位置 & 定義比例尺
+            const projection = d3.geoMercator().center([123, 24]).scale(5500)
+            const path = d3.geoPath(projection)
 
-const encrypt = Base64.stringify(
-    HmacSHA256((secret + uri + JSON.stringify(body) + nonce), secret)
-);
+            d3.select('g.counties')
+                .selectAll("path")
+                .data(topojson.feature(mapData, mapData.objects["COUNTY_MOI_1090820"]).features)
+                .enter().append("path")
+                .attr("d", path);
 
-const headers = {
-    headers: {
-        "Content-Type": "application/json",
-        "X-LINE-ChannelId": channelId,
-        "X-LINE-Authorization-Nonce": nonce,
-        "X-LINE-Authorization": encrypt,
-    }
-};
-
-axios.post(URL, body, headers)
-    .then((res) => console.log(res.data))
-    .catch((err) => console.log(err))
+            d3.select('path.county-borders')
+                .attr("d", path(topojson.mesh(mapData, mapData.objects["COUNTY_MOI_1090820"], function (a, b) { return a !== b; })));
+        })
+        .catch(err => console.log(err))
+}
+fetchTopo()
+drawMap(taiwanTopo)
