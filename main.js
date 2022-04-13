e52a7f25efd4b6a457d25648eee11cfacf47c980
leave3310 <sampisam3@gmail.com>
2022-04-13 14:09:31 +0800

    Complete change year to chinese

--- main.js
+++ main.js
@@ -1,86 +1,79 @@
-const API_URL = 'https://hex-escape-room.herokuapp.com'
-const CORS_URL = 'https://kylen-cors-anywhere.herokuapp.com'
-let resData = []
-let sideDate = {}
+console.clear();
+// ===== DOM Elements ===== //
+const universalDate = document.querySelector("#datepicker");
+const twDate = document.querySelector("#datepickerTW");
 
-const dateFormat = (time) => {
-    const event = new Date(time)
-    return event.toLocaleString()
-}
 
-const appendAside = () => {
-    const exist = document.getElementsByClassName('aside-news').length > 0
-    if (exist) $('.aside-news').remove()
-    
-    $('.article-list').append(`
-            <div class="col-12 col-md-6 col-lg-3 aside-news"> 
-                <div class="sticky-top">
-                    <button class="btn btn-outline-secondary shadow border-0">
-                        <i class="bi bi-caret-left-fill"></i> 返回
-                    </button>
-                    <h2 class="mt-3">${sideDate.title}</h2>
-                    <span>${dateFormat(sideDate.publishedAt)}</span>
-                    <img src="${sideDate.urlToImage}" class="img-fluid" alt="">
-                    <p>${sideDate.description}
-                    </p>
-                    <a href="${sideDate.url}" target="blank">看更多</a>
-                </div>
-            </div>
-    `)
+// ===== English Calendar ===== //
+const datePicker = flatpickr(universalDate, {
+    enableTime: true,
+    defaultDate: "today",
+    time_24hr: false,
+    minDate: "1911.01.01",
+    maxDate: "01,01,2030",
+    dateFormat: "d-m-Y",
+    altInput: true,
+    altFormat: "F j ,Y. D h:iK",
+});
 
-    $(".btn").click(() => {
-        $(".aside-news").remove()
-    })
-}
 
-const cardClick = (event) => {
-    $(".sticky-top").remove()
+// ===== 中文 Calendar ===== //
+// Reference: https://tipsfordev.com/flatpickr-adding-dropdown-to-change-year
+const SELF_DROP_YEAR = "flatpickr-selfmade-years"
+const momentDateFormat = "YYYY-M-DD-LT";
+const datePickerTW = flatpickr(twDate, {
+    locale: "zh_tw",
+    mode: "range",
+    minDate: "01,01,1911",
+    maxDate: "01,01,2030",
+    dateFormat: momentDateFormat,
+    formatDate: convertDateFormat,
+    onReady: function (selectedDates, dateStr, instance) {
 
-    $.ajax({
-        type: "GET",
-        url: `${CORS_URL}/${API_URL}/api/cors/news/${event.data.id}`,
-        dataType: "json",
-        success: (res) => {
-            sideDate = res.data
-            appendAside()
-        },
-        error: (err) => {
-            console.log(err);
+        // Step1: hide original year input
+        const oldYear = instance.currentYearElement
+        const childrenDom = oldYear.parentNode.childNodes
+        for (let i = 0; i < childrenDom.length; i++) {
+            childrenDom[i].style.display = "none"
         }
-    })
 
-}
+        // Step2: create dropdown select
+        const dropdown = document.createElement("select")
+        const minDate = moment(instance.config.minDate)
+        const maxDate = moment(instance.config.maxDate)
 
-const appendCard = () => {
-    $(".news-list").append(resData.map(item => {
-        return `
-            <div class="col">
-                <div id="card-${item.id}" class="card bg-dark text-white card-gradient">
-                    <img src="${item.urlToImage}" class="card-img"
-                        alt="${item.title}">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">${item.title}</h5>
-                        <p class="card-text">${dateFormat(item.publishedAt)}</p><a href="#"
-                            class="stretched-link"></a>
-                    </div>
-                </div>                    
-            </div>
-        `
-    }))
-    resData.forEach(item => (
-        $(`#card-${item.id}`).bind("click", { id: item.id }, cardClick)
-    ))
-}
+        for (let i = minDate.year(); i <= maxDate.year(); i++) {
+            const childNode = document.createElement("option")
+            childNode.value = i
+            childNode.text = `民國 ${i - 1911}`
+            childNode.className="flatpickr-monthDropdown-month"
+            dropdown.appendChild(childNode)
+        }
 
-$.ajax({
-    type: "GET",
-    url: `${API_URL}/api/cors/news`,
-    dataType: "json",
-    success: (res) => {
-        resData = res.data
-        appendCard()
+        // Step3: add event listener for select
+        dropdown.addEventListener('change', (event) => {            
+            instance.currentYear = parseInt(event.target.value)
+            instance.redraw()
+        })
+        //Step4: add classes and id to yearSelect and append it to yearEl
+        dropdown.id = SELF_DROP_YEAR
+        dropdown.className = "flatpickr-monthDropdown-months"
+        
+        console.log(instance.currentYear.value);
+        dropdown.value = instance.currentYear
+        oldYear.parentNode.appendChild(dropdown)
     },
-    error: (err) => {
-        console.log(err);
-    }
-})
+    onMonthChange: function (selectedDates, dateStr, instance) {
+        // update custom select year value
+        
+        document.getElementById(SELF_DROP_YEAR).value = instance.currentYear
+    },
+});
+
+// convert date format
+function convertDateFormat(dateObj, formatString, locale) {
+    const year = moment(dateObj).year() - 1911;
+    const dateArr = moment(dateObj).format(`民國 ${year}年 MM月 DD日`);
+    
+    return dateArr;
+}
